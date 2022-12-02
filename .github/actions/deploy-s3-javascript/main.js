471aacc8693e1fa8d778384e4fa2794533138803
neimv <neimv.zatara@gmail.com>
2022-12-02 14:23:16 -0600

    adding js action

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,10 @@
+
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const github = require('@actions/github');
+
+function run() {
+    core.notice('Hello from my custom JavaScript Action!');
+}
+
+run();
