dbf608e0b6c4af1281c147522a9faa82a14200bc
neimv <neimv.zatara@gmail.com>
2022-12-02 14:53:33 -0600

    some deploy

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,9 +1,18 @@
 
 const core = require('@actions/core');
 const exec = require('@actions/exec');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 
 function run() {
+    // get some input values
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: false });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // upload files
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`echo "echo aws ${distFolder} ${bucket} --region ${bucketRegion}"`);
+
     core.notice('Hello from my custom JavaScript Action!');
 }
 
